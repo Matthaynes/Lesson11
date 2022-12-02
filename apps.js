58a872462cc917f4d7ffefe19bfe984846dfa5ba
Matthaynes <124mhaynes@frhsd.com>
2022-12-02 11:03:17 -0500

    more files

--- apps.js
+++ apps.js
@@ -39,6 +39,21 @@ addForm.addEventListener('submit', function(e){
 const hideBox = document.querySelector('#hide');
 hideBox.addEventListener('change', function(e){
   if(hideBox.checked){
-    
+list.style.display = "none";
+  }else{
+    list.style.display = "initial";
   }
+});
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book){
+const title = book.firstElementChild.textContent;
+if(title.toLowerCase().indexOf(term)!=-1){
+  book.style.display = "block";
+} else {
+  book.style.display= "none";
 }
+})
+});
